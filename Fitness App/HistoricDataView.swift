30af792c91c58dd8feb728457a084e7333e4d0a6
Rahil Gandhi <708rahil@gmail.com>
2025-11-27 18:29:45 -0500

    View made

---
+++ Fitness App/HistoricDataView.swift
@@ -0,0 +1,18 @@
+//
+//  HistoricDataView.swift
+//  Fitness App
+//
+//  Created by Rahil Gandhi on 2025-11-27.
+//
+
+import SwiftUI
+
+struct HistoricDataView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    HistoricDataView()
+}
--- Fitness App/HomeView.swift
+++ Fitness App/HomeView.swift
@@ -9,16 +9,68 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        VStack {
-            Text("Home Page")
-                .font(.largeTitle)
-                .padding()
-
-            Text("Welcome to the Home Page!")
+        ScrollView(showsIndicators: false){
+            VStack{
+                Text("Welcome")
+                    .font(.largeTitle)
+                    .padding()
+                HStack{
+                    Spacer()
+                    
+                    VStack {
+                        VStack(alignment: .leading, spacing: 8){
+                            Text("Calories")
+                                .font(.callout)
+                                .bold()
+                                .foregroundColor(.red)
+                            
+                            Text("123 kcal")
+                                .bold()
+                        }
+                        
+                        VStack(alignment: .leading, spacing: 8){
+                            Text("Active")
+                                .font(.callout)
+                                .bold()
+                                .foregroundColor(.green)
+                            
+                            Text("52 mins")
+                                .bold()
+                        }
+                        
+                        VStack(alignment: .leading, spacing: 8){
+                            Text("Stand")
+                                .font(.callout)
+                                .bold()
+                                .foregroundColor(.blue)
+                            
+                            Text("8 hour")
+                                .bold()
+                        }
+                        
+                        
+                        
+                    }
+                    Spacer()
+                    ZStack{
+                        ZStack{
+                            Circle()
+                                .stroke(.red.opacity(0.3),lineWidth: 20)
+                            Circle()
+                                .trim(from:0,to:0.3)
+                                .stroke(style: StrokeStyle(lineWidth:20))
+                                .rotation(.degrees(-90))
+                            
+                            
+                        }
+                    }
+                }
+                
+            }
+            
         }
     }
 }
-
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
