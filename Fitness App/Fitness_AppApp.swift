b62e5464c88e20fd8548338aa9d3d25a72afea72
Rahil Gandhi <708rahil@gmail.com>
2025-11-27 16:24:45 -0500

    Initial Commit

---
+++ Fitness App/Fitness_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_AppApp.swift
+//  Fitness App
+//
+//  Created by Rahil Gandhi on 2025-11-27.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
