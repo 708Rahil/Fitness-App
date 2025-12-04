c1c7ec8d2ef4d564b00726bacbb3a23a057c7b9d
Rahil Gandhi <708rahil@gmail.com>
2025-12-03 19:44:04 -0500

    Healthkit testing

--- Fitness App/Home/HomeViewModel.swift
+++ Fitness App/Home/HomeViewModel.swift
@@ -9,6 +9,9 @@ import SwiftUI
 import Combine
 class HomeViewModel: ObservableObject {
     
+    let healthManager = HealthManager.shared
+
+    
     var calories:Int = 123
     var active:Int = 30
     var stand:Int = 8
@@ -25,6 +28,38 @@ class HomeViewModel: ObservableObject {
         Workout(id: 1, title: "Bench Press", image: "figure.strengthtraining.traditional", date: "Aug 3", duration: "45 min", calories: "592 kcal", tintColour: .blue),
         Workout(id: 2, title: "Walk", image: "figure.walk", date: "Aug 5", duration: "30 min", calories: "212 kcal", tintColour: .red),
         Workout(id: 3, title: "Swimming", image: "figure.pool.swim", date: "Aug 9", duration: "30 min", calories: "640 kcal", tintColour: .cyan)]
+    
+    init() {
+        Task{
+            do {
+                try await healthManager.requestHealthKitAccess()
+                healthManager.fetchTodayCaloriesBurned{
+                    result in switch result {
+                    case .success(let success):
+                        print(success)
+                    case .failure(let failure):
+                        print(failure.localizedDescription)
+                        
+                    }
+                }
+                
+                healthManager.fetchTodayStandHours {
+                    result in switch result {
+                    case .success(let success):
+                        print(success)
+                    case .failure(let failure):
+                        print(failure.localizedDescription)
+                        
+                    }
+                }
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        
+        
+        
+    }
     
     
 }
